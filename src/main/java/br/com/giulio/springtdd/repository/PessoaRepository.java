6f5a6f5a39d297f6535b97b5a7eb66e9e06a6ef8
Giulio Bernardi <giulioccbernardi@gmail.com>
2022-06-30 17:04:22 -0300

    Adicionei JPA nas Models e JpaRepository na classe de Repository

--- src/main/java/br/com/giulio/springtdd/repository/PessoaRepository.java
+++ src/main/java/br/com/giulio/springtdd/repository/PessoaRepository.java
@@ -1,11 +1,12 @@
 package br.com.giulio.springtdd.repository;
 
 import br.com.giulio.springtdd.model.Pessoa;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface PessoaRepository {
-    Pessoa save(Pessoa pessoa);
+public interface PessoaRepository extends JpaRepository<Pessoa, Long> {
+
 
     Optional<Pessoa> findByCpf(String cpf);
 
